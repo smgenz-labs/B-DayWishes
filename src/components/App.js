095e78a936c43eeec337ffadae5d6744679e9f63
Sameer Saini <38816886+soyo-kaze@users.noreply.github.com>
2021-05-01 04:05:16 +0000

    some coded added

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "../style/App.css"
+
+const App = () => {
+  return (
+    <>
+      <div className="coming__soon">
+          <h1>Coming Soon..</h1>
+      </div>
+    </>
+  );
+};
+
+export default App;
