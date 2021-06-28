a8d7dcdbaad6f5138f20af41be8224e14263024d
soyo-kaze <07sameersaini@gmail.com>
2021-06-28 19:37:43 +0530

    some addition

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,39 +1,73 @@
 import React from "react";
-import Card from './Card';
-import {Container,Col,Row} from 'react-bootstrap';
-import AssignmentTurnedInIcon from '@material-ui/icons/AssignmentTurnedIn';
-import CakeIcon from '@material-ui/icons/Cake';
-import MouseIcon  from '@material-ui/icons/Mouse';
-import '../styles/Home.css'
+import Card from "./Card";
+import { Container, Col, Row } from "react-bootstrap";
+import AssignmentTurnedInIcon from "@material-ui/icons/AssignmentTurnedIn";
+import CakeIcon from "@material-ui/icons/Cake";
+import MouseIcon from "@material-ui/icons/Mouse";
+import "../styles/Home.css";
+import bday from "./assets/Wavy_Lst-01_Single-04.webp";
 
-const Home = () =>{
-
-	return (
-		<>
-			<span className="heading">
-				<p>BirthDay Wishes</p>
-			</span>
-			<Container style={{height:"80vh"}}>
-			<Row className="align-items-center" style={{height:"100%"}} >
-				<Col md>
-					<Card  num="1" next="" heading="1. Click on Create" text="To get started click on the start button and fill out the info">
-						<MouseIcon style={{ fontSize: 35 }}/>
-					</Card>
-				</Col>
-				<Col md>
-					<Card  num="2" next="second__card" heading="2. Customize" text="Select the design, date of birth of the receiver, pics and music">
-					<AssignmentTurnedInIcon style={{ fontSize: 35 }}/>
-					</Card>
-				</Col>
-				<Col md>
-					<Card  num="3" next="third__card" heading="3. Finalize" text="A unique link will be created that you can share">
-					<CakeIcon style={{ fontSize: 35 }}/>
-					</Card>
-				</Col>
-			</Row>
-			</Container>
-		</>
-	)
-}
+const Home = () => {
+  return (
+    <>
+      <span className="heading">
+        <p>BirthDay Wishes</p>
+      </span>
+      <Container className="home__land">
+        <Row className="home__row">
+          <Col
+            style={{ display: "flex", flexDirection: "column" }}
+            className="head__holder"
+          >
+            <span className="upper__heading">Create Virtual Cards</span>
+            <span className="lower__heading">For your loved ones</span>
+          </Col>
+          <Col className="image__holder">
+            <img src={bday} />
+          </Col>
+        </Row>
+      </Container>
+      <div className="button__holder">
+        <a href="#info">
+          <button>↓</button>
+        </a>
+      </div>
+      <Container id="info" style={{ height: "80vh" }}>
+        <Row className="align-items-center" style={{ height: "100%" }}>
+          <Col md>
+            <Card
+              num="1"
+              next=""
+              heading="1. Click on Create"
+              text="To get started click on the start button and fill out the info"
+            >
+              <MouseIcon style={{ fontSize: 35 }} />
+            </Card>
+          </Col>
+          <Col md>
+            <Card
+              num="2"
+              next="second__card"
+              heading="2. Customize"
+              text="Select the design, date of birth of the receiver, pics and music"
+            >
+              <AssignmentTurnedInIcon style={{ fontSize: 35 }} />
+            </Card>
+          </Col>
+          <Col md>
+            <Card
+              num="3"
+              next="third__card"
+              heading="3. Finalize"
+              text="A unique link will be created that you can share"
+            >
+              <CakeIcon style={{ fontSize: 35 }} />
+            </Card>
+          </Col>
+        </Row>
+      </Container>
+    </>
+  );
+};
 
 export default Home;
