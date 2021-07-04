461ebb5ea7fc47cab419126a71d4098cc6aa136f
soyo-kaze <07sameersaini@gmail.com>
2021-07-04 17:56:32 +0530

    firebase stuff

---
+++ src/firebaseConfig.js
@@ -0,0 +1,13 @@
+import firebase from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA3qz8DGs7ggBneH8JKwrZ_SdYascb41v0",
+  authDomain: "b-daywishes.firebaseapp.com",
+  projectId: "b-daywishes",
+  storageBucket: "b-daywishes.appspot.com",
+  messagingSenderId: "937325615629",
+  appId: "1:937325615629:web:2eb3c2accee404ccfbd344",
+  measurementId: "G-87TE6QDS0L",
+};
+
+const firebaseInit = firebase.initializeApp(firebaseConfig);
